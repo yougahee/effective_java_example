7272fa70c701b5c882233447e56606b20a593bf6
가희 <ahrfus34@gmail.com>
2021-10-02 17:16:35 +0900

    item 44

---
+++ src/chapter7/Test.java
@@ -0,0 +1,11 @@
+package chapter7;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+public class Test extends LinkedHashMap{
+    public static void main(String[] args) {
+
+    }
+}
